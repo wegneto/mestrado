a865435c8fcade51ea863b93641ecabe26092c4a
Wilson Guimarães <wegneto@gmail.com>
2016-10-23 16:54:15 +0100

    Criação do teste para a classe BooleanMatrix.

---
+++ 1º ano/engenharia de software/Assignment 2/Algorithms/src/testsuitegroup90/BooleanMatrixTest.java
@@ -0,0 +1,35 @@
+package testsuitegroup90;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import DataStructures.BooleanMatrix;
+
+public class BooleanMatrixTest {
+	
+	private BooleanMatrix booleanMatrix;
+
+	@Before
+	public void setUp() throws Exception {
+		booleanMatrix = new BooleanMatrix(2, 2);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		booleanMatrix = null;
+	}
+
+	@Test
+	public void testGetAndSet() {
+		booleanMatrix.set(0, 0, true);
+		booleanMatrix.set(0, 1, false);
+		booleanMatrix.set(1, 0, true);
+		booleanMatrix.set(1, 1, false);
+		
+		assertEquals(true, booleanMatrix.get(0, 0));
+	}
+
+}
