5c1f0efc575524bc60b8a2341dc2596413b1ab21
Wilson Guimarães <wegneto@gmail.com>
2016-10-30 10:19:31 +0000

    Criação do test suite

---
+++ 1º ano/engenharia de software/Assignment 2/Algorithms/src/testsuitegroup90/TestSuite.java
@@ -0,0 +1,14 @@
+package testsuitegroup90;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ArrayListTest.class, BinarySearchTest.class, BinarySearchTreeTest.class, BooleanMatrixTest.class,
+		CountingSortTest.class, DynamicStackTest.class, GrahamScanTest.class, HashMapTest.class, HeapSortTest.class,
+		InsertionSortTest.class, LinkedListTest.class, MatrixTest.class, MaxHeapTest.class, MaximumSubarrayTest.class,
+		MergeSortTest.class, PointTest.class, QuickSortTest.class, SelectionSortTest.class, StackTest.class })
+public class TestSuite {
+
+}
